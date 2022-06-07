001dbbc9b8e2d4731dd4c23611a66aa95c217e88
elisiomualumene <elisiomualumene@gmail.com>
2022-06-07 21:20:28 +0100

    :memo: form layout with chakra done

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import { Button } from '@chakra-ui/react'
+import {
+  FormControl,
+  FormLabel,
+  Input,
+  Button,
+  Stack,
+  Center
+} from '@chakra-ui/react'
 
 function App() {
   return (
-    <>
-    Hello World
-    <Button colorScheme='pink'>Enviar</Button>
-    </>
+  <form>
+    <FormControl>
+          <Center h='100vh'>
+            <Stack spacing={3}>
+              <FormLabel htmlFor='email'>Email address</FormLabel>
+              <Input id='email' type='email' placeholder="Insira o seu E-mail" size='lg' width="80"/>
+    
+              <FormLabel htmlFor='password'>Password</FormLabel>
+              <Input id='password' type='password' placeholder="Insira a sua Password" size='lg'  width="80"/>
+              <Button colorScheme="red" width='80' type='submit'>Login</Button>
+            </Stack>
+          </Center>
+    </FormControl>
+  </form>
   )
 }
 
